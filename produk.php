c20c60ce27f454c6d615e307e4d2599963c87d31
Muhamad Fawwazt <57528446+MuhamadF@users.noreply.github.com>
2021-09-23 15:40:36 +0700

    upload-video-3

--- produk.php
+++ produk.php
@@ -1,8 +1,30 @@
-<? php
+<?php
 
 class Produk {
+    public  $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 10;
 
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 50000;
+
+$produk4 = new Produk();
+$produk4->judul = "Judulbaru";
+$produk4->penulis = "mf";
+$produk4->penerbit = "mf";
+$produk4->harga = 25000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Buku :" . $produk4->getLabel();
+
+?>
